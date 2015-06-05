c49b6b3865341f4b537f327c5aea1c8e9a762909
John Keith <johnkeith@outlook.com>
2015-06-05 06:52:05 -0400

    flash notice after registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
+  	flash[:notice] = "Your account has been created! Now please fill out your profile information."
     edit_profile_path(resource.id)
   end
 end
